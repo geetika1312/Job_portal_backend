afd77cbe3dae2f701bf326c832c8a4b4afee3705
geetika1312 <geetikabajpai03@gmail.com>
2026-03-26 17:25:42 +0530

    fix vercel routing

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true
-};
+// const corsOptions = {
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // const PORT = process.env.PORT || 3000;
 
