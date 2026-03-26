6ed0f0d7bf26a840895768f256c2df16e3834d88
geetika1312 <geetikabajpai03@gmail.com>
2026-03-26 17:36:57 +0530

    fixes

--- index.js
+++ index.js
@@ -16,14 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// const corsOptions = {
-//     origin: 'http://localhost:5173',
-//     credentials: true
-// };
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://job-portal-backend-mauve.vercel.app'],
+    credentials: true
+};
 
-// app.use(cors(corsOptions));
-
-app.use(cors());
+app.use(cors(corsOptions));
 
 // const PORT = process.env.PORT || 3000;
 
@@ -36,10 +34,6 @@ app.use((req, res, next) => {
     next();
 })
 // api's
-
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
@@ -50,4 +44,4 @@ app.use("/api/v1/application", applicationRoute);
 //     console.log(`Server running at port ${PORT}`);
 // })
 
-export default app;
+module.exports = app;
